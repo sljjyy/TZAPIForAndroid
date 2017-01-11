295393947857360d781eca88cf484f00448f833c
yeszoey <2323280981@qq.com>
2017-01-11 16:53:23 +0800

    文件下载修改

--- tzapi4android/src/main/java/com/tianzunchina/android/api/network/download/CallBackListener.java
+++
@@ -1,9 +0,0 @@
-package com.tianzunchina.android.api.network.download;
-
-/** TODO 名称太过通用 意义不明确
- * Created by zwt on 2016/12/20.
- */
-
-public interface CallBackListener {
-    void callback(int percent);
-}
